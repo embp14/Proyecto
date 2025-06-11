342371800fd7f3c0a4e3d5653c75e5fc77d91daf
embp14 <erick.bravo@est.ucacue.edu.ec>
2025-06-10 21:21:01 -0500

    Remove usuarios GUI

--- src/proyectobd/ParametrosGenerales/MenuPrincipalController.java
+++ src/proyectobd/ParametrosGenerales/MenuPrincipalController.java
@@ -5,16 +5,12 @@
 package proyectobd.ParametrosGenerales;
 
 import dao.ConectorBD;
-import dao.UsuarioDAO;
-import dto.UsuarioDTO;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -46,22 +42,6 @@ public class MenuPrincipalController implements Initializable {
         
     }
     
-    public void call_ListarUsuarios(){
-        try{
-                // Genero el stage
-                Stage stage_Lst_Usuarios=new Stage();
-                //Cargamos la interface gráfica para la lista de usuarios:
-                Parent root=FXMLLoader.load(getClass().getResource("/proyectobd/UsuariosGui/Lst_Usuarios_Gui.fxml"));
-                // Establecemos el título de la ventana
-                stage_Lst_Usuarios.setTitle("Gestión de usuarios del sistema");
-                //Establecemos la escena que se mostrará en el escenario
-                stage_Lst_Usuarios.setScene(new Scene(root));
-                //Mostramos el escenario
-                stage_Lst_Usuarios.show();
-        } catch (Exception ex){
-            fu.MostrarAlertas("Error del sistema", ex.toString());
-        }
-    }
 
     public void call_ListarVendedores(){
         try{
