91147c9df77ce8389ecd96ea8c777fd0f8706508
embp14 <erick.bravo@est.ucacue.edu.ec>
2025-06-12 21:56:44 -0500

    Add delete operation for roles

--- src/dao/RolDAO.java
+++ src/dao/RolDAO.java
@@ -88,4 +88,20 @@ public class RolDAO {
             return 0;
         }
     }
+
+    public int EliminarRol(int id){
+        try{
+            String sql = "DELETE FROM roles WHERE id=?";
+            ConectorBD ConnBD = new ConectorBD();
+            PreparedStatement ps = ConnBD.AbrirConexionBD().prepareStatement(sql);
+            ps.setInt(1, id);
+            int registros = ps.executeUpdate();
+            fu.MostrarAlertas("Información del sistema", "Rol eliminado. Registros afectados: "+registros);
+            ConnBD.CerrarConexionBD();
+            return registros;
+        }catch(Exception ex){
+            fu.MostrarAlertas("Error del sistema", ex.toString());
+            return 0;
+        }
+    }
 }
